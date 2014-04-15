996be411daeac5eebbd47471402aa952ce1e389f
Rainer Oehlmann <rainer.oehlmann@gmx.de>
2014-04-15 14:58:24 +0200

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe User do
   
   # The following code gets run before each example.
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
   # Makes @user the default subject of the test example.
   subject { @user }
@@ -12,6 +15,10 @@ describe User do
   # conveniently creates a list of the methods all User objects should respond to.
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   # Tests if the element fullfills all the requirements specified in models/user.rb
   it { should be_valid }
@@ -66,7 +73,58 @@ describe User do
 
     it { should_not be_valid }
   end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "missmatch" }
+    it { should_not be_valid }
+  end
+
+    describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+    
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      # specify is synonymous with it and can be used when it 
+      # wouldn't sound natural.
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
