9c2aec1d6dc41e1ea28710dba39cb46bc84d81f7
Rainer Oehlmann <rainer.oehlmann@gmx.de>
2014-04-23 16:03:36 +0200

    Add users index with pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user,  only: [:edit, :update]
+  before_action :signed_in_user,  only: [:index, :edit, :update]
   before_action :correct_user,    only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+    # params[:page] is generated automatically by will_paginate (in the view?)
+  end
 
   def show
   	@user = User.find(params[:id])
@@ -43,8 +48,11 @@ class UsersController < ApplicationController
     # Before filters
 
     def signed_in_user
+      unless signed_in?
+        store_location
       # Uses a shortcut for flash[:notice] - but doesnt work for error or success key
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do 	# tells FG that info is for a User model object.
-  	name		"Michael Hartl"
-  	email		"michael@example.com"
-  	password	"foobar"
+  	sequence(:name)  { |n| "Person #{n}" }
+  	sequence(:email) { |n| "person_#{n}@example.com" }
+  	password "foobar"
   	password_confirmation	"foobar"
   	# This definitions allows us to create a User factory in the tests
   	# with let(:user) { FactoryGirl.create(:user) }
